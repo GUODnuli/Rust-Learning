45aeb82ea05270bade2edb94c72f2f9d12f14e55
Zhuangxiaosen <624115642@qq.com>
2023-10-31 22:50:16 +0800

    修复variables作业

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 fn main() {
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("The 3h in seconds is {}", THREE_HOURS_IN_SECONDS);
@@ -13,19 +12,3 @@ fn main() {
 
     println!("The value of x is: {}", x);
 }
-=======
-fn main() {
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    println!("The 3h in seconds is {}", THREE_HOURS_IN_SECONDS);
-
-    let x = 5;
-    println!("THe value of x is: {}", x); 
-
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
-
-    println!("The value of x is: {}", x);
-}
->>>>>>> def5ed8c33e35007c31adda8c594035b2256c8a4
