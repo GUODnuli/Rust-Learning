e58e15434aba52b0cc67b6fcc87a6b6eae2c160c
Zhuangxiaosen <624115642@qq.com>
2023-10-31 22:46:48 +0800

    修复--force push

--- function/target/debug/deps/function-f6f207b250708dff.d
+++ function/target/debug/deps/function-f6f207b250708dff.d
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 /home/zhuangxiaosen/projects/function/target/debug/deps/function-f6f207b250708dff: src/main.rs
 
 /home/zhuangxiaosen/projects/function/target/debug/deps/function-f6f207b250708dff.d: src/main.rs
-=======
-/home/zhuangxiaosen/Rust-Learning/function/target/debug/deps/function-f6f207b250708dff: src/main.rs
-
-/home/zhuangxiaosen/Rust-Learning/function/target/debug/deps/function-f6f207b250708dff.d: src/main.rs
->>>>>>> def5ed8c33e35007c31adda8c594035b2256c8a4
 
 src/main.rs:
--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 fn main() {
     println!("Hello, world!");
 }
-=======
-fn main() {
-    println!("Hello, world!");
-}
->>>>>>> def5ed8c33e35007c31adda8c594035b2256c8a4
+
