8f5a26a856da90a644c4ead4b9764db638e7f15d
Xiaosen Zhuang <624115642@qq.com>
2023-11-09 00:24:00 +0800

    Delete collections directory

--- collections/target/debug/deps/collections-d416bbdaee0ccc8c.d
+++
@@ -1,5 +0,0 @@
-/home/zhuangxiaosen/projects/collections/target/debug/deps/collections-d416bbdaee0ccc8c: src/main.rs
-
-/home/zhuangxiaosen/projects/collections/target/debug/deps/collections-d416bbdaee0ccc8c.d: src/main.rs
-
-src/main.rs:
