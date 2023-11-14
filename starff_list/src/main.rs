483a4d82d4cf0fea5799a9a9f9b70a7d5ffe70e1
Zhuangxiaosen <624115642@qq.com>
2023-11-14 22:15:40 +0800

    23.11.14：常见集合课后练习，完成！

--- starff_list/src/main.rs
+++ starff_list/src/main.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 
 fn main() {
     println!("You can input like \"Add Sam to Engineering\" to add starff.");
-    println!("Or you can input \"get <apartment-name>\" to get all starff name in <apartment-name>");
+    println!("Or you can input \"get <department-name>\" to get all starff name in <department-name>");
     println!("And you can input \"quit\" to exit this program.");
     let mut starff_map: HashMap<String, String> = HashMap::new();
     loop {
@@ -17,28 +17,49 @@ fn main() {
             break;
         }
 
-        let operate = Regex::new(r"([\D]+)")
+        let operate = Regex::new(r"([a-zA-Z]+)")
             .expect("Failed to initialize regex rule.");
         match operate.captures(input) {
             Some(caps) => {
-                let operate_math = caps.get(1).unwrap().as_str().to_string();
-                println!("{}", operate_math);
+                let operate_type = caps.get(1).unwrap().as_str().to_string();
+                if operate_type == "Add" {
+                    add_employee(input, &mut starff_map);
+                } else if operate_type == "Get" {
+                    get_employee(input, &mut starff_map);
+                }
             },
             None => {},
         }
-        
-        let re_input = Regex::new(r"Add ([a-zA-Z]*) to ([a-zA-Z]*)")
-            .expect("Failed to initialize regex rule.");        
-        match re_input.captures(input) {
-            Some(caps) => {
-                let name = caps.get(1).unwrap().as_str().to_string();
-                let apartment = caps.get(2).unwrap().as_str().to_string();
+    }
+}
 
-                println!("{}: {}", apartment, name);
-                starff_map.entry(name).or_insert(apartment);
-            },
-            None => println!("Syntax error.\nPlease input like \"Add <name> to <apartment>\"."),
-        }
-        println!("{:?}", starff_map);
+fn add_employee(input: &str, starff_map: &mut HashMap<String, String>) {
+    let re_input = Regex::new(r"Add ([a-zA-Z]+) to ([a-zA-Z]+)")
+            .expect("Failed to initialize regex rule.");        
+    match re_input.captures(input) {
+        Some(caps) => {
+            let name = caps.get(1).unwrap().as_str().to_string();
+            let department = caps.get(2).unwrap().as_str().to_string();
+
+            println!("{}: {}", department, name);
+            starff_map.entry(name).or_insert(department);
+        },
+        None => println!("Syntax error.\nPlease input like \"Add <name> to <department>\"."),
+    }
+    println!("{:?}", starff_map);
+}
+
+fn get_employee(input: &str, starff_map: &mut HashMap<String, String>) {
+    let re_input = Regex::new(r"Get ([a-zA-Z]+)")
+        .expect("Failed to initialize regex rule.");
+    match re_input.captures(input) {
+        Some(caps) => {
+            let department = caps.get(1).unwrap().as_str().to_string();
+
+            for (key, value) in starff_map.iter().filter(|&(_, v)| v == &department) {
+                println!("{} in {}", key, &department);
+            }
+        },
+        None => println!("Syntax error.\nPlease input like \"Get <department>\"."),
     }
 }
