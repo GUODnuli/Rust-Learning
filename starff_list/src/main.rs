3c95b01443ca7b613e9a20430343ea85b5e623a5
zhuangxiaosen <624115642@qq.com>
2023-11-14 13:58:21 +0800

    23.11.14：常见集合课后练习，员工列表，部分。更新ignore文件,Readme文件。

--- starff_list/src/main.rs
+++ starff_list/src/main.rs
@@ -4,26 +4,41 @@ use std::collections::HashMap;
 
 fn main() {
     println!("You can input like \"Add Sam to Engineering\" to add starff.");
-    println!("Or you can input <apartment-name> to get ");
+    println!("Or you can input \"get <apartment-name>\" to get all starff name in <apartment-name>");
     println!("And you can input \"quit\" to exit this program.");
-    // let mut starff_map: HashMap<,> = HashMap::new();
+    let mut starff_map: HashMap<String, String> = HashMap::new();
     loop {
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line.");
         let input = input.trim();
+        if input == "quit" {
+            break;
+        }
+
+        let operate = Regex::new(r"([\D]+)")
+            .expect("Failed to initialize regex rule.");
+        match operate.captures(input) {
+            Some(caps) => {
+                let operate_math = caps.get(1).unwrap().as_str().to_string();
+                println!("{}", operate_math);
+            },
+            None => {},
+        }
         
-        let re_input = Regex::new(r"Add ([a-zA-Z]*) to ([a-zA-Z]*)").expect("Syntax error.");
+        let re_input = Regex::new(r"Add ([a-zA-Z]*) to ([a-zA-Z]*)")
+            .expect("Failed to initialize regex rule.");        
         match re_input.captures(input) {
             Some(caps) => {
-                let name = caps.get(1);
-                let apartment = caps.get(2);
-                println!("{:?}  {:?}", name, apartment);
-                // starff_map.entry(name).or_insert(apartment);
+                let name = caps.get(1).unwrap().as_str().to_string();
+                let apartment = caps.get(2).unwrap().as_str().to_string();
+
+                println!("{}: {}", apartment, name);
+                starff_map.entry(name).or_insert(apartment);
             },
-            _ => {},
+            None => println!("Syntax error.\nPlease input like \"Add <name> to <apartment>\"."),
         }
-        // println!("{:?}", starff_map);
+        println!("{:?}", starff_map);
     }
 }
