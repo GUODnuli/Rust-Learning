769ede05faa0037dc976c50c9d0308825957b076
zhuangxiaosen <624115642@qq.com>
2023-11-17 18:45:34 +0800

    23.11.17：泛型，部分。

--- generic_learn/src/main.rs
+++ generic_learn/src/main.rs
@@ -1,10 +1,19 @@
-struct Point<T> {
+#![allow(unused)]
+
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    
+}
+
+enum Car<T, E> {
+    Suv(T),
+    Lorry(E),
 }
 fn main() {
-    let integer = Point { x: 5, y: 10 };
-    let float = Point { x: 1.0, y: 4.0};
 //     let number_list = vec![34, 50, 25, 100, 65];
 
 //     let result = largest(&number_list);
@@ -26,6 +35,12 @@ fn main() {
 //     }
 
 //     largest
+
+// Type generic
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0};
+// can't compile
+    let wont_work = Point { x: 5, y: 4.0 };
 }
 
 // fn largest_i32(list: &[i32]) -> i32 {
