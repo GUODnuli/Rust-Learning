2c6e96fd8fd5c1b0bdf0e62d64696cfa37df2410
Zhuangxiaosen <624115642@qq.com>
2023-11-18 13:42:34 +0800

    23.11.18：泛型数据类型，完成。

--- generic_learn/src/main.rs
+++ generic_learn/src/main.rs
@@ -1,12 +1,25 @@
 #![allow(unused)]
 
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
 struct Point<T, U> {
     x: T,
     y: U,
 }
 
+// One generic_type implementations
+// impl<T> Point<T> {
+//     fn x(&self) -> &T {
+//         &self.x
+//     }
+// }
+
 impl<T, U> Point<T, U> {
-    
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point { x: self.x, y: other.y }
+    }
 }
 
 enum Car<T, E> {
@@ -40,7 +53,16 @@ fn main() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0};
 // can't compile
-    let wont_work = Point { x: 5, y: 4.0 };
+    // let wont_work = Point { x: 5, y: 4.0 };
+// One generic_type implementations
+    // let p = Point { x: 5, y: 10 };
+    // println!("p.x = {}", p.x());
+// Mix generic_type
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: "c" };
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
 
 // fn largest_i32(list: &[i32]) -> i32 {
