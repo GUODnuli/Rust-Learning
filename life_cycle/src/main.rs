e8927df623b51e5aa46e13c31f31e1e8ee735ac6
zhuangxiaosen <624115642@qq.com>
2023-11-21 13:34:03 +0800

    23.11.21：生命周期与引用有效性，完成。

--- life_cycle/src/main.rs
+++ life_cycle/src/main.rs
@@ -1,9 +1,16 @@
-fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+#![allow(unused)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
@@ -12,4 +19,20 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     } else {
         y
     }
+}
+fn main() {
+    // life_cycle_tag
+    // let string1 = String::from("abcd");
+    // let result;
+    // {
+    //     let string2 = String::from("xyz");
+    //     result = longest(string1.as_str(), string2.as_str());
+    // }
+    // println!("The longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
 }
