8966176541970795dfff210825ac9359521bdc66
Zhuangxiaosen <624115642@qq.com>
2023-11-28 22:57:39 +0800

    23.11.28：重构以改进模块化与错误处理，部分。

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,17 @@
-use std::{ fs, env };
+use std::{ fs, env, process };
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Sonmething went wrong reading the file.");
 
     println!("With text:\n{}", contents);
@@ -19,9 +22,14 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments.");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config { query, filename }
+        Ok(Config { query, filename })
+    }
 }
