9650f0599b2cf2380ecd3ac007ba18e682f5fd01
zhuangxiaosen <624115642@qq.com>
2023-12-01 13:56:04 +0800

    23.12.1：闭包：可以捕获其环境的匿名函数，完成。

--- closures_learn/src/main.rs
+++ closures_learn/src/main.rs
@@ -2,8 +2,8 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+    let simulated_user_specified_value = 26;
+    let simulated_random_number = 26;
     
     generate_workout(
         simulated_user_specified_value,
@@ -11,23 +11,50 @@ fn main() {
     );
 }
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = simulated_expensive_calculation(intensity);
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_result
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_result
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -35,7 +62,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_result
+                expensive_result.value(intensity)
             );
         }
     }
