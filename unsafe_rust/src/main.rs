e99b8e3b6ffeb6a117aee44ec69891afce25787a
zhuangxiaosen <xz.ganbaruibro@gmail.com>
2023-12-18 15:45:54 +0800

    23.12.18

--- unsafe_rust/src/main.rs
+++ unsafe_rust/src/main.rs
@@ -1,3 +1,4 @@
+use std::slice;
 fn main() {
     let mut num = 5;
 
@@ -18,5 +19,32 @@ fn main() {
         dangerous();
     }
 
-    dangerous();
+// =============================================== //
+
+    let mut num = vec![1, 2, 3, 4, 5, 6];
+    {
+        let r = &mut num[..];
+
+        let (a, b) = r.split_at_mut(3);
+        assert_eq!(a, &mut [1, 2, 3]);
+        assert_eq!(b, &mut [4, 5, 6]);
+    }
+
+    {
+        let p = &mut num[..];
+        let (c, d) = my_split_at_mut(p, 3);
+        assert_eq!(c, &mut [1, 2, 3]);
+        assert_eq!(d, &mut [4, 5, 6]);
+    }
+}
+
+fn my_split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid < len);
+
+    unsafe {
+        (slice::from_raw_parts_mut(ptr, mid),slice::from_raw_parts_mut(ptr.add(mid), len - mid))
+    }
 }
