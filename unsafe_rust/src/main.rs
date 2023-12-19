4cadc44ea64c1a985026c96a7ed97090b82a1c5c
zhuangxiaosen <624115642@qq.com>
2023-12-19 16:59:46 +0800

    23.12.19：不安全的Rust，完成。

--- unsafe_rust/src/main.rs
+++ unsafe_rust/src/main.rs
@@ -1,4 +1,16 @@
 use std::slice;
+
+// static variables
+static HELLO_WORLD: &str = "Hello, world!";
+static mut COUNTER: u32 = 0;
+
+// union
+#[repr(C)]
+union MyUnion {
+    f1: u32,
+    f2: f32,
+}
+
 fn main() {
     let mut num = 5;
 
@@ -36,6 +48,37 @@ fn main() {
         assert_eq!(c, &mut [1, 2, 3]);
         assert_eq!(d, &mut [4, 5, 6]);
     }
+// =============================================== //
+
+    // really unsafe code!
+    let address = 0x012345usize;
+    let r = address as *mut i32;
+
+    let slice: &[i32] = unsafe {
+        slice::from_raw_parts_mut(r, 10000)
+    };
+// =============================================== //
+
+    // use static variables
+    println!("name is: {}", HELLO_WORLD);
+
+// =============================================== //
+
+    // change static variables
+    add_to_count(3);
+
+    unsafe {
+        println!("COUNTER: {}", COUNTER);
+    }
+
+// =============================================== //
+
+    // use union
+    let u = MyUnion { f1: 1 };
+    let f = unsafe {
+        u.f1
+    };
+    println!("MyUnion.f1: {}", f);
 }
 
 fn my_split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
@@ -47,4 +90,27 @@ fn my_split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
     unsafe {
         (slice::from_raw_parts_mut(ptr, mid),slice::from_raw_parts_mut(ptr.add(mid), len - mid))
     }
+}
+
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+#[no_mangle]
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from C!");
+}
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
+unsafe trait Foo {
+    // methods go here
+}
+
+unsafe impl Foo for i32 {
+    // method implementations go here
 }
