65db52b50f516a0e57b73481fe090c3d95b11749
Zhuangxiaosen <624115642@qq.com>
2023-12-24 23:26:38 +0800

    23.12.24：高级trait，部分。

--- advanced_trait/src/main.rs
+++ advanced_trait/src/main.rs
@@ -56,10 +56,16 @@ impl Human {
     }
 }
 
+trait Animal {
+    fn baby_name() -> String;
+}
+
 fn main() {
     assert_eq!(Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
         Point { x: 3, y: 3 });
     
     let person = Human;
+    Pilot::fly(&person);
+    Wizard::fly(&person);
     person.fly();
 }
