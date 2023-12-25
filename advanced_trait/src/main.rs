ff5bcdd84f564d0bfe24d3a63940d80871a6bd4a
zhuangxiaosen <624115642@qq.com>
2023-12-25 18:28:50 +0800

    23.12.25：高级trait，部分。

--- advanced_trait/src/main.rs
+++ advanced_trait/src/main.rs
@@ -1,4 +1,5 @@
 use std::ops::Add;
+use std::fmt;
 
 #[derive(Debug, PartialEq)]
 struct Point {
@@ -28,6 +29,8 @@ impl Add<Meters> for MilliMeters {
     }
 }
 
+// ==================================================== //
+
 trait Pilot {
     fn fly(&self);
 }
@@ -56,16 +59,51 @@ impl Human {
     }
 }
 
+// ==================================================== //
 trait Animal {
     fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
+// ==================================================== //
+
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
 }
 
 fn main() {
     assert_eq!(Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
         Point { x: 3, y: 3 });
-    
+
+// ==================================================== //
+
     let person = Human;
     Pilot::fly(&person);
     Wizard::fly(&person);
     person.fly();
+
+// ==================================================== //
+    
+    println!("A baby dog is colled a {}", <Dog as Animal>::baby_name());
 }
