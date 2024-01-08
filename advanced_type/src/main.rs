7743c4a137cfc108dfb587839c17972072f23467
zhuangxiaosen <624115642@qq.com>
2024-01-08 13:57:17 +0800

    24.1.8：高级类型，完成。

--- advanced_type/src/main.rs
+++ advanced_type/src/main.rs
@@ -1,5 +1,8 @@
 #![allow(unused)]
 
+use std::io::Error;
+use std::fmt;
+
 fn main() {
     type Kilometers = i32;
 
@@ -40,3 +43,31 @@ fn returns_short_type() -> Thunk {
     let f: Thunk = Box::new(|| println!("hi"));
     f
 }
+
+// ========================================== //
+
+// unuse alias
+// pub trait Write {
+//     fn write(&mut self, buf: &[u8]) -> Result<usize, Error>;
+//     fn flush(&mut self) -> Result<(), Error>;
+
+//     fn write_all(&mut self, buf: &[u8]) -> Result<(), Error>;
+//     fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<(), Error>;
+// }
+
+pub trait Write {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize>;
+    fn flush(&mut self) -> std::io::Result<()>;
+
+    fn write_all(&mut self, buf: &[u8]) -> std::io::Result<()>;
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> std::io::Result<()>;
+}
+
+// ========================================== //
+
+fn bar() -> ! {
+    loop {
+        // --snip--
+    }
+}
+
