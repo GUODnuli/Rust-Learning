f14b9db3f9c38b56f35de6dcd419d1a251ef8a2c
zhuangxiaosen <624115642@qq.com>
2024-01-10 17:35:15 +0800

    24.1.10：将单线程服务器变为多线程服务器，完成。

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,7 +1,9 @@
 use std::thread;
+use std::sync::{ mpsc, Arc, Mutex };
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -15,14 +17,19 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         ThreadPool {
-            workers
+            workers,
+            sender,
         }
     }
 
@@ -30,7 +37,9 @@ impl ThreadPool {
         where
             F: FnOnce() + Send + 'static
     {
+        let job = Box::new(f);
 
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -40,9 +49,16 @@ pub struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        assert!(id > 0);
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                println!("Worker {} got a job; executing.", id);
+
+                job();
+            }
+        });
 
         Worker {
             id,
@@ -50,3 +66,5 @@ impl Worker {
         }
     }
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
